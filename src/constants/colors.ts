6cb9195997a780d29046693e2666680dd556e062
gudwh14 <alxious@naver.com>
2022-10-11 22:49:00 +0900

    feat : #4 AppColor

---
+++ src/constants/colors.ts
@@ -0,0 +1,18 @@
+const colors = {
+  background: '#F2F2F2',
+  primary1: '#1b64da',
+  primary2: '#3182f6',
+  primary3: '#4593FC1F',
+  primary4: '#C9E2FF',
+  text1: '#000000',
+  text2: '#333D4B',
+  text3: '#4E5968',
+  text4: '#6b7684',
+  tag1: '#5D6CB8',
+  tag2: '#56C3BD',
+  tag3: '#995CE8',
+  tag4: '#79C75E',
+  tag5: '#E1A64E',
+};
+
+export default colors;
