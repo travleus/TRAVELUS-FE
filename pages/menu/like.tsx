e3fd374a1bd4c997e23b1208acf2ddfd2af96e6e
gudwh14 <alxious@naver.com>
2022-10-17 15:11:59 +0900

    feat : #62 [Layout] My Liked Place Page

---
+++ pages/menu/like.tsx
@@ -0,0 +1,37 @@
+import { NextPage } from 'next';
+import { Container, FlexRowCenterContainer, MainContainer, TopContainer } from '@components/Container';
+import BackButton from '@components/BackButton';
+import { css } from '@emotion/react';
+import Text from '@components/Text';
+import LikeItem from '@components/LikeItem';
+import Footer from '@components/Footer';
+
+const Like: NextPage = () => {
+  return (
+    <Container>
+      <BackButton />
+      <TopContainer>
+        <FlexRowCenterContainer>
+          <img
+            css={css`
+              margin-right: 15px;
+            `}
+            width={20}
+            height={20}
+            src={'/icons/heart_active.png'}
+            alt={'like_icon'}
+          />
+          <Text typographyType={'t3'} fontWeight={600}>
+            찜 장소
+          </Text>
+        </FlexRowCenterContainer>
+      </TopContainer>
+      <MainContainer>
+        <LikeItem />
+      </MainContainer>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Like;
