ad3d7ea9812797faa22504b423b0b7a6f6207db9
gudwh14 <alxious@naver.com>
2022-10-17 15:07:45 +0900

    feat : #69 [Component] PlaceItem Refactoring

--- src/components/PlaceItem.tsx
+++ src/components/PlaceItem.tsx
@@ -2,37 +2,17 @@ import { css } from '@emotion/react';
 import Text from '@components/Text';
 import TagItem from '@components/TagItem';
 import colors from '@constants/colors';
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
 
 function PlaceItem() {
+  const router = useRouter();
+
   return (
-    <div
-      css={css`
-        display: flex;
-        align-items: center;
-        margin-bottom: 12px;
-        cursor: pointer;
-      `}>
-      <img
-        css={css`
-          object-fit: cover;
-          border-radius: 5px;
-          margin-right: 10px;
-        `}
-        width={100}
-        height={100}
-        src={'/dummy.jpeg'}
-        alt={'item'}
-      />
-      <div
-        css={css`
-          height: 100px;
-        `}>
-        <div
-          css={css`
-            display: flex;
-            align-items: center;
-            margin-bottom: 5px;
-          `}>
+    <Wrapper>
+      <PlaceImage src={'/dummy.jpeg'} alt={'item'} />
+      <ContentWrapper onClick={() => router.push('/hotel/1')}>
+        <TitleWrapper>
           <Text
             css={css`
               margin-right: 5px;
@@ -43,13 +23,41 @@ function PlaceItem() {
             에코랜드 테마파크
           </Text>
           <TagItem />
-        </div>
+        </TitleWrapper>
         <Text typographyType={'t7'} color={colors.text3}>
           에코랜드는 제주특별 자치도 제주시 조천읍 대흘리에 위치한 테마파크이다.
         </Text>
-      </div>
-    </div>
+      </ContentWrapper>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+
+const PlaceImage = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 5px;
+  margin-right: 10px;
+`;
+
+const ContentWrapper = styled.div`
+  flex: 1;
+  height: 100px;
+  margin-right: 15px;
+  cursor: pointer;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 5px;
+`;
 
 export default PlaceItem;
